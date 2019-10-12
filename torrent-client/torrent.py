5ea043dc93abad6df27320c9f2915b5b345418a3
Sarah Gaidi <gaidi.sarah@gmail.com>
2019-10-12 14:05:24 +0300

    Initial implementation of Torrent class and example input files

---
+++ torrent-client/torrent.py
@@ -0,0 +1,14 @@
+
+
+class Torrent:
+
+    def __init__(self, path):
+        pass
+
+    @classmethod
+    def from_magnet_link(cls, magnet_link):
+        pass
+
+    @classmethod
+    def from_data(cls, data_path):
+        pass

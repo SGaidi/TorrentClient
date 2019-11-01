d92c710cf0bbd1d12e1d617b2771db2a019baacf
Sarah Gaidi <gaidi.sarah@gmail.com>
2019-11-01 22:46:00 +0200

    Implemented a similiar structure for Torrent class, needs research in BitTorrent specifications

--- torrentclient/torrent.py
+++ torrentclient/torrent.py
@@ -1,25 +1,42 @@
+import bencode
+import hashlib
 
 
 class Torrent:
+    """Class for extracting meta-info from a torrent file.
 
-    # Constructors
+    Reference: https://wiki.theory.org/index.php/BitTorrentSpecification#Metainfo_File_Structure
+    """
 
-    def __init__(self, path):
-        self.path = path
-
-    @classmethod
-    def from_magnet_link(cls, magnet_link):
-        pass
-
-    @classmethod
-    def from_data(cls, data_path):
-        pass
-
-    # Basic Attributes
+    def __init__(self, path: str):
+        self.file = open(path, 'r').read()
+        self.metadata = bencode.bdecode(self.file)
 
     @property
-    def name(self):
-        pass
+    def info(self) -> dict:
+        """a dictionary that describes the file(s) of the torrent
+        TODO: has 2 modes (1 or multiple files), use a design pattern / class for it"""
+        return self.metadata['info']
 
-    def __str__(self):
-        return "I'm a torrent at {}".format(self.path)
+    @property
+    def announce(self) -> str:
+        """announce URL of the tracker"""
+        return self.metadata['']
+
+"""
+hashcontents = bencode.bencode(metadata['info'])
+import hashlib
+digest = hashlib.sha1(hashcontents).digest()
+import base64
+b32hash = base64.b32encode(digest)
+b32hash
+
+# should be => magnet:?xt=urn:btih:CT76LXJDDCH5LS2TUHKH6EUJ3NYKX4Y6
+
+# this below can be passed to MagnetLink.encode() / torrent2magnet?
+# see: http://dan.folkes.me/2012/04/converting-a-magnet-link-into-a-torrent/
+params = {'xt': 'urn:btih:%s' % b32hash,
+    'dn': metadata['info']['name'],
+    'tr': metadata['announce'],
+    'xl': metadata['info']['length']}
+"""
